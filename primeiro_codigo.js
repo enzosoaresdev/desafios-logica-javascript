fdf9cce0ff1df088b1b209a3b554cb741a32e298
enzosoaresdev <enzo.ferreira.soares@hotmail.com>
2025-07-20 20:10:33 -0300

    chore: Add comentário.

--- primeiro_codigo.js
+++ primeiro_codigo.js
@@ -1,7 +1,8 @@
+//Meu primeiro código em JavaScript
+
 let meuNome = "Enzo";
 let minhaIdade = 20;
 
 let fraseDeApresentacao = "Meu nome é " + meuNome + " e eu tenho " + minhaIdade + " anos. Estou começando minha jornada para a liberdade.";
 
 console.log(fraseDeApresentacao);
-// Teste final de atualização - 06/07/2025.
